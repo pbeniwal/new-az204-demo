9bd6800bc7ffe3bbfafa7eb98f67f9a3b89dcb8e
prashant <pbeniwal@yahoo.com>
2022-05-05 09:06:21 +0530

    added

---
+++ Redis/Rediscache/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using StackExchange.Redis;
+using System.Threading.Tasks;
+
+namespace Rediscache
+{
+    class Program
+    {
+        static string connectionString = "myredisdemo.redis.cache.windows.net:6380,password=TEITSkfCb2EXTei19DYB5YoiZVC7kfXNSAzCaLPTLXQ=,ssl=True,abortConnect=False";
+
+        static async Task Main(string[] args)
+        {
+            // The connection to the Azure Cache for Redis is managed by the ConnectionMultiplexer class.
+            using (var cache = ConnectionMultiplexer.Connect(connectionString))
+            {
+                IDatabase db = cache.GetDatabase();
+                // Snippet below executes a PING to test the server connection
+                var result = await db.ExecuteAsync("ping");
+                Console.WriteLine($"PING = {result.Type} : {result}");
+                // Call StringSetAsync on the IDatabase object to set the key "test:key" to the value "100"
+                //bool setValue = await db.StringSetAsync("test:key", "200");
+                //Console.WriteLine($"SET: {setValue}");
+                //StringGetAsync takes the key to retrieve and return the value
+                string getValue = await db.StringGetAsync("test:key");
+                Console.WriteLine($"GET: {getValue}");
+            }
+        }
+    }
+}
---
+++ SvcBus/producer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace producer
+{
+    class Program
+    {
+        
+        // connection string to your Service Bus namespace
+        static string connectionString = "Endpoint=sb://mydemosvcbus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ihVlTNJetxedy6pA128fyrpdUpTosJ+Mg9PkYl+VOW4=";
+        // name of your Service Bus topic
+        static string queueName = "demo";
+
+        // the client that owns the connection and can be used to create senders and receivers
+        static ServiceBusClient client;
+        // the sender used to publish messages to the queue
+        static ServiceBusSender sender;
+        // number of messages to be sent to the queue
+        private const int numOfMessages = 10;        
+        
+        static async Task Main()
+        {
+            // Create the clients that we'll use for sending and processing messages.
+            client = new ServiceBusClient(connectionString);
+            sender = client.CreateSender(queueName);
+
+            // create a batch
+            using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                // try adding a message to the batch
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                {
+                    // if an exception occurs
+                    throw new Exception($"Exception {i} has occurred.");
+                }
+            }
+            try
+            {
+                // Use the producer client to send the batch of messages to the Service Bus queue
+                await sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+            Console.WriteLine("Press any key to end the application");
+            Console.ReadKey();
+        }
+    }
+}
